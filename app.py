42557a64653dfd0728fb0cc3307872e928fbbf67
Sunday Ezeh <ezesunday4378@gmail.com>
2024-08-20 08:51:09 +0100

    thumbnail adjusted

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-from key import cohere_api_key
-# Cohere Model
+from urllib.parse import urlparse, parse_qs
+from dotenv import load_dotenv
 from langchain_community.chat_models import ChatCohere
 from youtube_transcript_api import YouTubeTranscriptApi
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import requests
+
+load_dotenv()
 
 prompt_template = """
 You will be providing summary and the details of YouTube videos. Use the transcript below text to give a professional 
@@ -17,50 +20,71 @@ Put the details here
 {transcript_text}
 """
 
-# Getting transcript from the Youtube video
 def extract_transcript(YT_video_url):
     try:
-        video_id = YT_video_url.split("=")[1]
+        video_id = get_video_id(YT_video_url)
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
         transcript = ''
         for i in transcript_text:
             transcript += ' ' + i['text']
         return transcript
-    
     except Exception as e:
         raise e
 
-# Getting the summary
 def generate_summary(transcript_text, prompt_template):
     class CustomChatCohere(ChatCohere):
         def _get_generation_info(self, response):
-            # Custom handling of generation info
             generation_info = {}
             if hasattr(response, 'token_count'):
                 generation_info["token_count"] = response.token_count
-            # Add other attributes if needed
             return generation_info
 
         def get_num_tokens(self, text: str) -> int:
-            # Specify the model explicitly
             response = self.client.tokenize(text=text, model=self.model)
             return len(response.tokens)
 
-    llm = CustomChatCohere(cohere_api_key=cohere_api_key)
+    llm = CustomChatCohere()
     
     prompt = PromptTemplate(template=prompt_template, input_variables=["transcript_text"])
-    chain = LLMChain(prompt=prompt, llm=llm)
     
-    response = chain.run(transcript_text)
-    return response
+    chain = prompt | llm
+    
+    response = chain.invoke(transcript_text)
+    return response.content
+
+def get_video_id(youtube_url):
+    parsed_url = urlparse(youtube_url)
+    video_id = parse_qs(parsed_url.query).get('v')
+    if video_id:
+        return video_id[0]
+    return None
+
+def get_thumbnail_url(video_id):
+    resolutions = ['maxresdefault', 'sddefault', 'hqdefault', 'mqdefault', 'default']
+    for res in resolutions:
+        url = f"http://img.youtube.com/vi/{video_id}/{res}.jpg"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return url
+    return None  # Return None if no valid thumbnail is found
 
 st.set_page_config(page_title='Youtube Transcribe', page_icon='🤖')
 st.title('Youtube Transcript to Note Converter')
 youtube_link = st.text_input('Paste Youtube video link')
 
 if youtube_link:
-    video_id = youtube_link.split("=")[1]
-    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+    try:
+        video_id = get_video_id(youtube_link)
+        if video_id:
+            thumbnail_url = get_thumbnail_url(video_id)
+            if thumbnail_url:
+                st.image(thumbnail_url, use_column_width=True)
+            else:
+                st.error("Thumbnail not available.")
+        else:
+            st.error("Invalid YouTube link format. Please check the URL.")
+    except IndexError:
+        st.error("Invalid YouTube link format. Please check the URL.")
 
 if st.button("Get Video Summary"):
     transcript_text = extract_transcript(youtube_link)
